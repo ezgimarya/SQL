6b517e9c2660b50b6f77e21aac8b3f228d703385
ezgimarya <115090813+ezgimarya@users.noreply.github.com>
2022-10-08 15:52:36 +0300

    Update and rename LIKEANDILIKE to LIKEANDILIKE.sql

--- LIKEANDILIKE.sql
+++ LIKEANDILIKE.sql
@@ -1,19 +1,15 @@
-------------------
-SQL
-------------------
-
-Q1
+-- Q1
 SELECT * FROM country 
 WHERE country LIKE 'A%a' ;
 
-Q2
+-- Q2
 SELECT * FROM country 
 WHERE country LIKE '_____%n';
 
-Q3
+-- Q3
 SELECT title FROM film 
 WHERE title ILIKE '%T%%T%%T%%T%';
 
-Q4
+-- Q4
 SELECT * FROM film
 WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99 
