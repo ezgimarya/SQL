5febb8c69b2f1896eaf0aa50988986e1589cd379
ezgimarya <115090813+ezgimarya@users.noreply.github.com>
2022-10-08 15:54:33 +0300

    ORDERBYANDLIMIT.sql
    www.patika.dev

---
+++ ORDERBYANDLIMIT.sql
@@ -0,0 +1,19 @@
+
+-- Q1
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5 ;
+
+-- Q2
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length
+OFFSET 5
+LIMIT 5 ;
+
+-- Q3
+SELECT last_name, store_id FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4 ;
